0befa8360e8df15110f68a0c6b4a587b6cd1214f
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2023-10-08 20:37:45 +0800

    feat: add task demo file and base task runner

--- conf/docker/cli/shell/task/run.py
+++ conf/docker/cli/shell/task/run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
 import sys
 import yaml
 from yaml.loader import SafeLoader
@@ -9,10 +10,21 @@ if __name__ == '__main__':
     print("Task: {0}".format(task))
     with open(task, 'r') as f:
         data = yaml.load(f, Loader=SafeLoader)
-        print(yaml.dump(data))
-        print('---')
-        print(type(data))
-        print(data)
-        print('---')
+        #print(type(data))
+        #print(yaml.dump(data))
+        #print(data)
+        # log to name.log
         print(data['name'])
-        print(data['age'])
+        # algo exist, then execute algo.name with algo.param
+        if 'algo' in data:
+            aname = data['algo']['name'] if "name" in data['algo'] else ""
+            aparam = data['algo']['param'] if "param" in data['algo'] else ""
+            #print("asa exec {0} {1}".format(aname, aparam))
+            os.system("asa exec {0} {1}".format(aname, aparam))
+        else:
+            print("Task file don't have 'algo' key for execute algorithm.")
+        # result exist, when algo stdout put into that protocol.
+        #   - file
+        #   - callback function
+        #   - message queue
+        print(data['result'])
