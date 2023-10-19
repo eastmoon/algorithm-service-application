8b3ced2835049643136bf68bce88ee2f54dd6c51
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2023-10-19 14:51:57 +0800

    feat: add sub algorithm

---
+++ app/sub/main.py
@@ -0,0 +1,13 @@
+#@PARAME Parame1 Parame2
+#@DESC 這是減法演算法
+#@DESC Parame1 是第一個數值
+#@DESC Parame2 是第二個數值
+
+import sys
+import numpy as np
+
+if __name__ == '__main__':
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+    r = np.subtract(num1, num2)
+    print("Result: {0}".format(r))
