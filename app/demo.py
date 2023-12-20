1a10794d912f1fbf748079cf430cdbb3b427b7c5
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2023-12-21 01:00:49 +0800

    fix: algorithm parameter showing format error.

--- app/demo.py
+++ app/demo.py
@@ -1,3 +1,5 @@
+#@DESC 這是基礎演算法範例
+
 import numpy as np
 
 if __name__ == '__main__':
--- conf/docker/cli/shell/ls.sh
+++ conf/docker/cli/shell/ls.sh
@@ -1,28 +1,33 @@
 #@STOP-CLI-PARSER
 
 # Declare variable
+BLU="\033[36m"
+GRE="\033[1;32m"
+RED="\033[1;31m"
+END="\033[0m"
 
 # Declare function
 function show {
     ALGO_NAME=${1}
     FILE_PATH=${2}
+    echo ""
     if [ $(grep "#@PARAME" ${FILE_PATH} | wc -l) -eq 0 ];
     then
-        echo "${ALGO_NAME}"
+        echo-c ${GRE} ${END} "${ALGO_NAME}"
     else
         str=$(grep "#@PARAME" ${FILE_PATH} | sed -e "s/#@PARAME//g" | sed -e "s/^ //g")
-        echo "${ALGO_NAME} (${str})"
+        echo-c ${GRE} ${END} "${ALGO_NAME} : ${str} "
     fi
     if [ $(grep "#@DESC" ${FILE_PATH} | wc -l) -gt 0 ];
     then
         while read line; do
             desc=${line##*@DESC }
-            echo "  ${desc}"
+            echo "${desc}"
         done < <(grep "#@DESC" ${FILE_PATH})
     fi
 }
 function action {
-    echo-c "\033[36m" "\033[0m" Algorithm list:
+    echo-c ${BLU} ${END} Algorithm list:
     ## Search all python file in application directories
     for file in $(find ${APP_A_DIR} -maxdepth 1 -type f -iname "*.py");
     do
